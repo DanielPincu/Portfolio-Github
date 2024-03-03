910da3ec0a2ec77e07b1ac1a6073f9d48e4ba521
DanielPincu <ghepin01@easv365.dk>
2024-03-03 11:13:35 +0100

    nav updated

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,34 +1,31 @@
 <template>
-
-
-<label  class="inline-flex items-center cursor-pointer mt-20 ml-10 sticky top-[100px]">
-  <input type="checkbox" value="" class="sr-only peer">
-  <div @click="toggleDark()" class="relative w-11 h-6 bg-red-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-red-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-  
-</label>
+  <label class="inline-flex items-center cursor-pointer mt-20 ml-10 sticky top-[100px]">
+    <input type="checkbox" value="" class="sr-only peer">
+    <div @click="toggleDark()" class="relative w-11 h-6 bg-red-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-red-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  </label>
 
   <div class="mt-10 sticky top-[300px] mx-5 mr-5">
-    <div class="flex justify-evenly items-center rounded-full">
-      <a class="text-lg font-bold text-blue-900 cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700" @click="scrollTo('#home')">Home</a>
-    </div>
-
-    <div class="flex justify-evenly items-center my-5">
-      <a class="text-lg font-bold text-blue-900 cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700" @click="scrollTo('#portfolio')">Portfolio</a>
-    </div>
-
-    <div class="flex justify-evenly items-center my-5">
-      <a class="text-lg font-bold text-blue-900 cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700" @click="scrollTo('#about')">About</a>
-    </div>
-
-    <div class="flex justify-evenly items-center my-5">
-      <a class="text-lg font-bold text-blue-900 cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700" @click="scrollTo('#contact')">Contact</a>
+    <div v-for="(navItem, index) in navItems" :key="index" class="flex justify-evenly items-center my-5">
+      <a @click="scrollTo(navItem.href, $event)" class="text-lg font-bold text-blue-900 cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700">{{ navItem.label }}</a>
     </div>
   </div>
 </template>
 
 <script setup>
+import { useDark, useToggle } from "@vueuse/core"
 
-const scrollTo = (target) => {
+const navItems = [
+  { label: "Home", href: "#home" },
+  { label: "Portfolio", href: "#portfolio" },
+  { label: "About", href: "#about" },
+  { label: "Contact", href: "#contact" }
+]
+
+const isDark = useDark()  
+const toggleDark = useToggle(isDark)
+
+const scrollTo = (target, event) => {
+  event.preventDefault();
   const element = document.querySelector(target);
   if (element) {
     const offsetTop = element.offsetTop;
@@ -38,13 +35,4 @@ const scrollTo = (target) => {
     });
   }
 };
-
-import {useDark, useToggle}  from "@vueuse/core"
-
-
-
-    const isDark = useDark()  
-    const toggleDark = useToggle(isDark)
-    
-
 </script>
