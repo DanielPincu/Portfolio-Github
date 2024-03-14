d0169114bfd0350d8547ca57d797074b20f060c4
DanielPincu <ghepin01@easv365.dk>
2024-03-14 15:43:05 +0100

    Working! Yes

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -3,7 +3,7 @@ export const portfolioCards = [
       title: 'Trapholt',
       description: "I'm proud to present a project where I contributed and executed a comprehensive redesign for Trapholt Museum, renowned cultural institution in Kolding",
       category: 'Web Design',
-      imageUrl: 'src/assets/img/trap1.png',
+      imageUrl: '/assets/img/trap1.png',
       details: 'The website features intuitive navigation, immersive imagery, and interactive elements to enhance the user experience. Additionally, I integrated functionalities such as event listings, virtual tours, and online ticketing to streamline visitor engagement and accessibility',
       dynamicLink: 'https://www.danielpincu.com' // Dynamic link property
     },
@@ -12,7 +12,7 @@ export const portfolioCards = [
       title: 'Chatognito',
       description: "I'm proud to present a project where I contributed and executed a comprehensive redesign for Trapholt Museum, renowned cultural institution in Kolding, Denmark",
       category: 'Design',
-      imageUrl: 'src/assets/img/trap2.png',
+      imageUrl: '/assets/img/trap2.png',
       details: 'The website features intuitive navigation, immersive imagery, and interactive elements to enhance the user experience. Additionally, I integrated functionalities such as event listings, virtual tours, and online ticketing to streamline visitor engagement and accessibility',
       dynamicLink: 'src/assets/img/OK.mp4' // Dynamic link property
     },
@@ -21,7 +21,7 @@ export const portfolioCards = [
       title: 'InDesign',
       description: "I'm proud to present a project where I contributed and executed a comprehensive redesign for Trapholt Museum, renowned cultural institution in Kolding",
       category: 'Art', 
-      imageUrl: 'src/assets/img/trap1.png',
+      imageUrl: '/assets/img/trap1.png',
       details: 'The website features intuitive navigation, immersive imagery, and interactive elements to enhance the user experience. Additionally, I integrated functionalities such as event listings, virtual tours, and online ticketing to streamline visitor engagement and accessibility'
     },
   
@@ -29,7 +29,7 @@ export const portfolioCards = [
       title: 'InDesign',
       description: "I'm proud to present a project where I contributed and executed a comprehensive redesign for Trapholt Museum, renowned cultural institution in Kolding, Denmark",
       category: 'Art', 
-      imageUrl: 'src/assets/img/trap1.png',
+      imageUrl: '/assets/img/trap1.png',
       details: 'The website features intuitive navigation, immersive imagery, and interactive elements to enhance the user experience. Additionally, I integrated functionalities such as event listings, virtual tours, and online ticketing to streamline visitor engagement and accessibility'
     },
     {
@@ -43,7 +43,7 @@ export const portfolioCards = [
       title: 'InDesign',
       description: "I'm proud to present a project where I contributed and executed a comprehensive redesign for Trapholt Museum, renowned cultural institution in Kolding, Denmark",
       category: 'Art', 
-      imageUrl: 'src/assets/img/trap1.png',
+      imageUrl: '/assets/img/trap1.png',
       details: 'The website features intuitive navigation, immersive imagery, and interactive elements to enhance the user experience. Additionally, I integrated functionalities such as event listings, virtual tours, and online ticketing to streamline visitor engagement and accessibility'
     },
   ];
