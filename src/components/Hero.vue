34ec758dce050a62c13a80fafaa4792136ed9f92
DanielPincu <ghepin01@easv365.dk>
2024-03-15 10:43:29 +0100

    Nav check

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="lg:hidden sticky top-0 z-10 bg-blue-500 text-white dark:bg-red-500 -mx-5">
+  <div class="lg:hidden sticky top-0 z-10 bg-blue-500 text-white dark:bg-red-500 ">
     <div class="flex justify-between items-center mt-10 mx-5 sticky top-20">
       <div class="flex my-5">
         <div v-for="(navItem, index) in navItems" :key="index" class="mx-3">
           <a @click="scrollTo(navItem.href, $event)" class="text-lg font-bold cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700">{{ navItem.label }}</a>
         </div>
       </div>
-      <label class="inline-flex items-center cursor-pointer rotate-90">
+       <label class="inline-flex items-center cursor-pointer">
         <input type="checkbox" value="" class="sr-only peer" v-model="isDark">
         <div class="relative w-11 h-6 bg-red-700 rounded-full peer dark:bg-red-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
       </label>
