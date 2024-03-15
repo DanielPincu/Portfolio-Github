3f1dab090197660dcd9e9e971d3b251107a3ac58
DanielPincu <ghepin01@easv365.dk>
2024-03-15 10:37:45 +0100

    Button mobile responsive test

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,7 +6,7 @@
           <a @click="scrollTo(navItem.href, $event)" class="text-lg font-bold cursor-pointer hover:scale-110 hover:transition ease-in-out duration-700">{{ navItem.label }}</a>
         </div>
       </div>
-      <label class="inline-flex items-center cursor-pointer">
+      <label class="inline-flex items-center cursor-pointer rotate-90">
         <input type="checkbox" value="" class="sr-only peer" v-model="isDark">
         <div class="relative w-11 h-6 bg-red-700 rounded-full peer dark:bg-red-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
       </label>
