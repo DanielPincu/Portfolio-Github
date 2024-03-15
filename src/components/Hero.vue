423f0b0616d880ab55d440c84e7c9f0610aa6bb4
DanielPincu <ghepin01@easv365.dk>
2024-03-15 11:01:38 +0100

    Nav UPdated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -17,11 +17,11 @@
   
 
 
-  <div class="absolute inset-0 flex flex-col justify-center items-center text-center mb-20">
+  <div class="absolute inset-0 flex flex-col justify-center items-center text-center">
     <h1 class="text-sm md:text-xl pb-10 xl:text-3xl font-bold text-white">
       I’m Daniel Pincu <br> <span class="text-blue-500 dark:text-red-500">Front-end</span> Developer
     </h1>
-    <label class="inline-flex items-center cursor-pointer mb-10">
+    <label class="inline-flex items-center cursor-pointer">
         <input type="checkbox" value="" class="sr-only peer" v-model="isDark">
         <div class="relative w-11 h-6 bg-red-700 rounded-full peer dark:bg-red-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
       </label>
