0544dfea3190f3508cd6c86f85f1c247e060476f
DanielPincu <ghepin01@easv365.dk>
2024-03-23 08:42:53 +0100

    Modal updated

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,48 +1,65 @@
 <template>
-  <div :id="portfolioId">
-    <h1 class="text-center text-4xl pt-20 pb-10">Showcase</h1>
-    <!-- Filter dropdown -->
-    <div class="mb-4">
-      <label for="category" class="block text-sm font-medium text-gray-700">Filter by Category:</label>
-      <select id="category" v-model="selectedCategory" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-blue-100 dark:bg-red-100 rounded-md shadow-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:focus:ring-red-500 dark:focus:border-red-500">
-        <option value="" class="bg-yellow-700">All</option>
-        <option v-for="category in categories" :key="category">{{ category }}</option>
-      </select>
+  <div>
+    <div :id="portfolioId">
+      <h1 class="text-center text-4xl pt-20 pb-10">Showcase</h1>
+      <!-- Filter dropdown -->
+      <div class="mb-4">
+        <label for="category" class="block text-sm font-medium text-gray-700">Filter by Category:</label>
+        <select id="category" v-model="selectedCategory" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-blue-100 dark:bg-red-100 rounded-md shadow-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:focus:ring-red-500 dark:focus:border-red-500">
+          <option value="" class="bg-yellow-700">All</option>
+          <option v-for="category in categories" :key="category">{{ category }}</option>
+        </select>
+      </div>
     </div>
-  </div>
 
-  <!-- Portfolio cards -->
-  <div class="grid md:grid-cols-2 gap-10 mt-16">
-    <div v-for="(card, index) in filteredCards" :key="index" class="suitcase-card cursor-pointer" @click="toggleVisibility(index)">
-      <div class="suitcase-wrapper">
-        <div class="suitcase bg-blue-200 dark:bg-red-200 rounded-3xl overflow-hidden border-2 border-blue-300 shadow-xl">
-          <div class="suitcase-top bg-blue-400 dark:bg-red-400 p-4 relative">
-            <div class="handle bg-blue-600 dark:bg-red-700 w-6 h-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+    <!-- Portfolio cards -->
+    <div class="grid md:grid-cols-2 gap-10 mt-16">
+      <div v-for="(card, index) in filteredCards" :key="index" class="suitcase-card cursor-pointer" @click="toggleVisibility(index)">
+        <div class="suitcase-wrapper">
+          <div class="suitcase bg-blue-200 dark:bg-red-200 rounded-3xl overflow-hidden border-2 border-blue-300 shadow-xl">
+            <div class="suitcase-top bg-blue-400 dark:bg-red-400 p-4 relative">
+              <div class="handle bg-blue-600 dark:bg-red-700 w-6 h-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+            </div>
+            <div class="suitcase-body hover:bg-blue-300 dark:hover:bg-red-300 ease-out duration-500 bg-blue-200 xl:h-80 md:h-[450px] 2xl:h-64 dark:bg-red-200 p-4">
+              <h1 class="text-4xl text-gray-800 font-bold cursor-pointer mb-5 px-5 2xl:pl-10">{{ card.title }}</h1>
+              <p class="text-lg px-5 2xl:px-10">{{ card.description }}</p>
+            </div>
+            <div v-show="visibleDiv === index">
+              <a @click="openModalWithWebsite(card.dynamicLink)">
+                <img class="bg-blue-200 hover:scale-105 hover:border-4 hover:border-black dark:bg-red-200 ease-in-out duration-500" :src="card.imageUrl" alt="">
+              </a>
+              <p class="text-lg bg-blue-200 dark:bg-red-200 p-10">{{ card.details }}</p>
+            </div>
           </div>
-          <div class="suitcase-body hover:bg-blue-300 dark:hover:bg-red-300 ease-out duration-500 bg-blue-200 xl:h-80 md:h-[450px] 2xl:h-64 dark:bg-red-200 p-4">
-            <h1 class="text-4xl text-gray-800 font-bold cursor-pointer mb-5 px-5 2xl:pl-10">{{ card.title }}</h1>
-            <p class="text-lg px-5 2xl:px-10">{{ card.description }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal component -->
+    <div class="fixed inset-0 overflow-y-auto z-10" v-if="showModal">
+      <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center">
+        <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div class="inline-block bg-white rounded-3xl text-left overflow-hidden shadow-xl transform transition-all my-8 align-middle w-[1200px]">
+          <div class="bg-white px-4 pt-5 pb-4">
+            <iframe :src="modalWebsiteUrl" frameborder="0" allowfullscreen class="w-full h-[70vh]"></iframe>
           </div>
-          <div v-show="visibleDiv === index">
-            <a @click="openModalWithWebsite(card.dynamicLink)">
-              <img class="bg-blue-200 hover:scale-105 hover:border-4 hover:border-black dark:bg-red-200 ease-in-out duration-500" :src="card.imageUrl" alt="">
-            </a>
-            <p class="text-lg bg-blue-200 dark:bg-red-200 p-10">{{ card.details }}</p>
+          <div class="bg-gray-50 px-4 py-3 flex flex-row-reverse">
+            <button @click="showModal = false" type="button" class="my-5 inline-flex justify-center w-full rounded-full px-4 py-2 bg-gradient-to-r from-blue-400 to-blue-600 dark:from-red-400 dark:to-red-600 font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 ml-3 text-sm">
+              Close
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-  <!-- Modal component -->
-  <WebsiteModal v-if="showModal" :websiteUrl="modalWebsiteUrl" @closeModal="showModal = false" />
-
 </template>
 
 <script setup>
 import { ref, computed } from 'vue';
 import { portfolioCards } from '../modules/portfolio.js';
-import WebsiteModal from './Modal.vue';
 
 // Define reactive variables
 const selectedCategory = ref('');
@@ -76,5 +93,3 @@ const filteredCards = computed(() => {
 const portfolioId = 'portfolio';
 
 </script>
-
-
