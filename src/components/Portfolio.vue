c13d68948da3f5d6f6cda0d26afb417622cbf442
DanielPincu <ghepin01@easv365.dk>
2024-04-03 10:24:03 +0200

    portfolio mx

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -36,14 +36,14 @@
             </div>
             <div class="suitcase-body hover:bg-blue-300 dark:hover:bg-red-300 ease-out duration-500 bg-blue-200 h-60 dark:bg-red-200 p-4">
               <h1 class="text-4xl text-gray-800 font-bold cursor-pointer mb-5 2xl:pl-5">{{ card.title }}</h1>
-              <p class=" mb-5 2xl:px-5">{{ card.description }}</p>
+              <p class="mb-5 2xl:px-5">{{ card.description }}</p>
             </div>
             <div v-show="visibleDiv === index">
               <a @click="openWebsite(card.dynamicLink)">
                 <img class="bg-blue-200 border-t-2 border-b-2 border-transparent hover:border-blue-500 hover:dark:border-red-500 dark:bg-red-200 ease-in-out duration-100"
                      :src="card.imageUrl" alt="">
               </a>
-              <p class="bg-blue-200 dark:bg-red-200 p-10">{{ card.details }}</p>
+              <p class="bg-blue-200 dark:bg-red-200 m-5 2xl:px-5">{{ card.details }}</p>
             </div>
           </div>
         </div>
