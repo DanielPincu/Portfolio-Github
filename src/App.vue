f8c798f600e11ade88e5ae39b875f1c5b83dc60e
DanielPincu <ghepin01@easv365.dk>
2024-04-03 09:58:10 +0200

    AOS Mobile Disabled

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <script setup>
+  import CV from './components/CV.vue'
+  import Hero from './components/Hero.vue'
+  import Services from './components/Services.vue'
+  import Nav from './components/Nav.vue'
+  import Portfolio from './components/Portfolio.vue'
+  import About from './components/About.vue'
+  import Contact from './components/Contact.vue'
+  import Footer from './components/Footer.vue'
 
-import CV from './components/CV.vue'
-import Hero from './components/Hero.vue'
-import Services from './components/Services.vue'
-import Nav from './components/Nav.vue'
-import Portfolio from './components/Portfolio.vue'
-import About from './components/About.vue'
-import Contact from './components/Contact.vue'
-import Footer from './components/Footer.vue'
+  // Function to check if the device is a mobile device
+  const isMobileDevice = () => {
+    return window.innerWidth <= 768; // Adjust the breakpoint as needed
+  };
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import AOS CSS file
-AOS.init({
-    
-    once: true,
-});
-
-
-
-
-
+  // It stops the stupid animation on mobile devices
+  if (!isMobileDevice()) {
+    import('aos').then(module => {
+      const AOS = module.default;
+      import('aos/dist/aos.css').then(() => {
+        AOS.init({
+          once: true,
+        });
+      });
+    });
+  }
 </script>
 
 
