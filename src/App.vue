5d39cd7ea7327ddafdb8956b465601d094dd64f0
DanielPincu <ghepin01@easv365.dk>
2024-04-03 10:05:12 +0200

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<!-- <script setup>
+
+import CV from './components/CV.vue'
+import Hero from './components/Hero.vue'
+import Services from './components/Services.vue'
+import Nav from './components/Nav.vue'
+import Portfolio from './components/Portfolio.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS CSS file
+AOS.init({
+    
+    once: true,
+});
+
+
+
+
+
+</script> -->
+
 <script setup>
   import CV from './components/CV.vue'
   import Hero from './components/Hero.vue'
@@ -13,7 +37,7 @@
     return window.innerWidth <= 768; // Adjust the breakpoint as needed
   };
 
-  // It stops the stupid animation on mobile devices
+  // It disables the stupid animations on mobile devices
   if (!isMobileDevice()) {
     import('aos').then(module => {
       const AOS = module.default;
@@ -25,8 +49,6 @@
     });
   }
 </script>
-
-
 
 <template>
    
