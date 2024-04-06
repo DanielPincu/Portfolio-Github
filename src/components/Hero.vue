6f7f83d2393df63c7ffa6295eb903a4c04e7d80a
DanielPincu <ghepin01@easv365.dk>
2024-04-06 10:40:47 +0200

    Fancy Buttons

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -36,12 +36,34 @@
         <p class="text-xl md:text-3xl xl:text-5xl pl-3 mb-1 text-white">{{ welcomeText }}<span class="animate-pulse">|</span></p>
 
         <!-- CV Buttons -->
-        <div class="flex md:pt-5 2xl:pt-24">
+        <!-- <div class="flex md:pt-5 2xl:pt-24">
           <button @click="openModal(cvFetchLink, 'Download')" class="hidden dark:block cv-button bg-gradient-to-r from-blue-400 to-blue-600 dark:from-red-400 dark:to-red-600 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44 mr-2 md:mr-44 2xl:mr-64">{{ cvFetchText }}</button>
           <button @click="openModal(cvWatchLink, 'Watch')" class="block dark:hidden cv-button bg-gradient-to-r from-blue-400 to-blue-600 dark:from-red-400 dark:to-red-600 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44 mr-2 md:mr-44 2xl:mr-64">{{ cvWatchText }}</button>
           <button @click="openModal(cvWatchLink, 'Watch')" class="hidden dark:block cv-button bg-gradient-to-r from-blue-400 to-blue-600 dark:from-blue-400 dark:to-blue-600 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44">{{ cvWatchText }}</button>
           <button @click="openModal(cvFetchLink, 'Download')" class="block dark:hidden cv-button bg-gradient-to-r from-red-400 to-red-600 dark:from-red-400 dark:to-red-600 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44">{{ cvFetchText }}</button>
-        </div>
+        </div> -->
+
+        <div class="flex md:pt-5 2xl:pt-24">
+  <button @click="openModal(cvFetchLink, 'Download')" class="hidden dark:block button bg-red-500  shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44 mr-2 md:mr-44 2xl:mr-64">
+    <span>{{ cvFetchText }}</span>
+    <div class="liquid"></div>
+  </button>
+  <button @click="openModal(cvWatchLink, 'Watch')" class="block dark:hidden button bg-blue-500 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44 mr-2 md:mr-44 2xl:mr-64">
+    <span>{{ cvWatchText }}</span>
+    <div class="liquid2"></div>
+  </button>
+  <button @click="openModal(cvWatchLink, 'Watch')" class="hidden dark:block button bg-blue-500 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44">
+    <span>{{ cvWatchText }}</span>
+    <div class="liquid2"></div>
+  </button>
+  <button @click="openModal(cvFetchLink, 'Download')" class="block dark:hidden button bg-red-500 shadow-lg border-b-2 border-slate-50 text-white font-bold text-sm md:text-xl px-5 md:py-6 2xl:px-0 rounded-full w-38 md:w-44">
+    <span>{{ cvFetchText }}</span>
+    <div class="liquid"></div>
+  </button>
+</div>
+
+
+
       </div>
     </div>
 
